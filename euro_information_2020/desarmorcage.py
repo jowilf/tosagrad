02bf147b9957945c1c35d488a83eed1ae54d8a34
jowilf <hounonj@gmail.com>
2020-11-24 22:13:21 +0100

    mouchard

---
+++ euro_information_2020/desarmorcage.py
@@ -0,0 +1,7 @@
+import itertools
+
+n = int(input())
+init_regs = map(int, input().strip())
+instructions = [input().strip().split()]
+for comb in itertools.combinations(range(1, n + 1), ):
+    pass
